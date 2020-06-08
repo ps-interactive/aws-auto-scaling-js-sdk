4ed14c5c2c48699024446cd2d8073131c9b7ff5a
Tom Bell <tjbell@gmail.com>
2020-06-08 10:58:07 -0600

    feat: info.js

--- info.js
+++ info.js
@@ -1,4 +1,4 @@
-const { message } = require('./helpers.js')
+const _ = require('lodash')
 const AWS = require('aws-sdk')
 AWS.config.region = 'us-west-2';
 AWS.config.apiVersions = { ec2: '2016-11-15' };
@@ -9,7 +9,17 @@ const params = {
   Filters: [{Name: 'isDefault', Values: ['true']}]
 };
 
-ec2.describeVpcs(params, function(err, data) {
-  if (err) console.log(err, err.stack);
-  else     console.log(data);
+ec2.describeVpcs(params, (err, data) => {
+  if (err) { console.log(err, err.stack); }
+  else {
+    const vpcID = data.Vpcs[0].VpcId;
+    const subnetParams = { Filters: [{Name: "vpc-id", Values: [vpcID]}] };
+    ec2.describeSubnets(subnetParams, (err, data) => {
+      if (err) { console.log(err, err.stack); }
+      else {
+        console.log('VpcId:', vpcID)
+        _.map(_.sortBy(data.Subnets, 'AvailabilityZone'), item => console.log(`AvailabilityZone: ${item.AvailabilityZone}\tSubnetId: ${item.SubnetId}` ));
+      }
+    });
+  }
 });

ea049d1bd1ca576919c7247578f710482d0f81f7
Tom Bell <tjbell@gmail.com>
2020-06-09 16:44:39 -0600

    feat: use json files for vpc and subnets

--- auto.js
+++ auto.js
@@ -7,30 +7,39 @@ const autoScaling = new AWS.AutoScaling();
 const elb = new AWS.ELBv2();
 
 const createLaunchConfiguration = (name) => {
-  const userData = 'IyEvYmluL2Jhc2gNCmN1cmwgLXNMIGh0dHBzOi8vZGViLm5vZGVzb3VyY2UuY29tL3NldHVwXzE0LnggfCBzdWRvIC1FIGJhc2ggLQ0Kc3VkbyBhcHQtZ2V0IGluc3RhbGwgLXkgbm9kZWpzIGdpdA0KZ2l0IGNsb25lIGh0dHBzOi8vZ2l0aHViLmNvbS9wcy1pbnRlcmFjdGl2ZS9hd3MtYXV0by1zY2FsaW5nLWFwcA0KY2QgYXdzLWF1dG8tc2NhbGluZy1hcHANCm5wbSBpDQpucG0gcnVuIHN0YXJ0';
-  const params = {
-    ImageId: 'ami-09dd2e08d601bff67',
-    InstanceType: 't2.micro',
-    LaunchConfigurationName: name,
-    UserData: userData
-  };
-  autoScaling.createLaunchConfiguration(params, message);
+  const json = readJSON('UserData');
+  if (json) {
+    const params = {
+      LaunchConfigurationName: name,
+      ImageId: json.ImageId,
+      InstanceType: json.InstanceType,
+      UserData: json.UserData
+    };
+    autoScaling.createLaunchConfiguration(params, message);
+  } else {
+    console.log("There was an error reading the `UserData.json` file.")
+  }
 };
 
 const createLoadBalancer = (name) => {
-
-  const params = { Name: name, Subnets: subnets };
-  elb.createLoadBalancer(params, message);
-
+  const json = readJSON('Subnets');
+  if (json) {
+    const params = { Name: name, Subnets: json.Subnets };
+    elb.createLoadBalancer(params, message);
+  } else {
+    console.log("There was an error reading the `Subnets.json` file. Have you run the `setup` command?")
+  }
 };
 
 const createTargetGroup = (name) => {
-  getVpcId().then(vpc => {
-    const params = { Name: name, Port: 3000, Protocol: 'HTTP', VpcId: vpc };
+  const json = readJSON('Vpcs');
+  if (json) {
+    const params = { Name: name, Port: 3000, Protocol: 'HTTP', VpcId: json.Vpcs[0].VpcId };
     elb.createTargetGroup(params, message);
-  });
+  }  else {
+    console.log("There was an error reading the `Vpcs.json` file. Have you run the `setup` command?")
+  }
 };
-
 
 const createListener = () => {
   const targetGroupArn = readJSON('TargetGroups');
@@ -68,7 +77,6 @@ const createAutoScalingGroup = () => {
   } else {
     console.log('Have you created a Target Group?')
   }
-
 };
 
 const putScalingPolicy = () => {

681a219aa85608f690e2c4d066bd38fe54c50a54
Tom Bell <tjbell@gmail.com>
2020-06-10 15:56:08 -0600

    solution: removed

--- auto.js
+++ auto.js
@@ -1,120 +1,24 @@
 const { command, resourceName, linkedResourceName, message, readJSON, setup, sortSubnets } = require('./helpers.js');
 
-const AWS = require('aws-sdk');
-AWS.config.region = 'us-west-2';
+const createLaunchConfiguration = (name) => {};
 
-const autoScaling = new AWS.AutoScaling();
-const elb = new AWS.ELBv2();
+const createLoadBalancer = (name) => {};
 
-const createLaunchConfiguration = (name) => {
-  const json = readJSON('UserData');
-  if (json) {
-    const params = {
-      LaunchConfigurationName: name,
-      ImageId: json.ImageId,
-      InstanceType: json.InstanceType,
-      UserData: json.UserData
-    };
-    autoScaling.createLaunchConfiguration(params, message);
-  } else {
-    console.log("There was an error reading the `UserData.json` file.");
-  }
-};
+const createTargetGroup = (name) => {};
 
-const createLoadBalancer = (name) => {
-  const subnets = sortSubnets(readJSON('Subnets'));
-  if (subnets) {
-    const params = { Name: name, Subnets: subnets };
-    elb.createLoadBalancer(params, message);
-  } else {
-    console.log("There was an error reading the `Subnets.json` file. Have you run the `setup` command?");
-  }
-};
+const createListener = () => {};
 
-const createTargetGroup = (name) => {
-  const vpc = readJSON('Vpcs');
-  if (vpc) {
-    const params = { Name: name, Port: 3000, Protocol: 'HTTP', VpcId: vpc.Vpcs[0].VpcId };
-    elb.createTargetGroup(params, message);
-  }  else {
-    console.log("There was an error reading the `Vpcs.json` file. Have you run the `setup` command?");
-  }
-};
+const createAutoScalingGroup = (name, lcName) => {};
 
-const createListener = () => {
-  const targetGroup = readJSON('TargetGroups');
-  const loadBalancer = readJSON('LoadBalancers');
-  if (targetGroup && loadBalancer) {
-    const params = {
-      DefaultActions: [{
-        TargetGroupArn: targetGroup.TargetGroups[0].TargetGroupArn,
-        Type: 'forward'
-      }],
-      LoadBalancerArn: loadBalancer.LoadBalancers[0].LoadBalancerArn,
-      Port: 80,
-      Protocol: 'HTTP'
-    };
-    elb.createListener(params, message);
-  } else {
-    console.log('Have you created a Target Group and a Load Balancer?');
-  }
-};
-
-const createAutoScalingGroup = (name, lcName) => {
-  const targetGroup = readJSON('TargetGroups');
-  if (targetGroup) {
-    const params = {
-      AutoScalingGroupName: name,
-      AvailabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d'],
-      TargetGroupARNs: [ targetGroup.TargetGroups[0].TargetGroupArn ],
-      LaunchConfigurationName: lcName,
-      MaxSize: 2,
-      MinSize: 1
-    };
-    autoScaling.createAutoScalingGroup(params, message);
-  } else {
-    console.log('Have you created a Target Group?');
-  }
-};
-
-const putScalingPolicy = (name) => {
-  const params = {
-    AdjustmentType: 'ChangeInCapacity',
-    AutoScalingGroupName: name,
-    PolicyName: `${name}-policy`,
-    PolicyType: 'TargetTrackingScaling',
-    TargetTrackingConfiguration: {
-      TargetValue: 5,
-      PredefinedMetricSpecification: {
-        PredefinedMetricType: 'ASGAverageCPUUtilization'
-      }
-    }
-  };
-  autoScaling.putScalingPolicy(params, message);
-};
+const putScalingPolicy = (name) => {};
 
 switch (command) {
-  case 'setup':
-    setup();
-    break;
-  case 'config':
-    createLaunchConfiguration(resourceName);
-    break;
-  case 'load':
-    createLoadBalancer(resourceName);
-    break;
-  case 'target':
-    createTargetGroup(resourceName);
-  case 'listener':
-    createListener();
-    break;
-  case 'group':
-    createAutoScalingGroup(resourceName, linkedResourceName);
-    break;
-  case 'policy':
-    putScalingPolicy(resourceName);
-    break;
-  default:
-    console.error('Not a valid command!');
-    break;
+  case    'setup': setup(); break;
+  case   'config': createLaunchConfiguration(resourceName); break;
+  case     'load': createLoadBalancer(resourceName); break;
+  case   'target': createTargetGroup(resourceName); break;
+  case 'listener': createListener(); break;
+  case    'group': createAutoScalingGroup(resourceName, linkedResourceName); break;
+  case   'policy': putScalingPolicy(resourceName); break;
+  default        : console.error('Not a valid command!'); break;
 }
